70d785cef3cc07d507b8c2ac10ba158c19da0633
Tejas Shinde <91648258+Tejas-0612@users.noreply.github.com>
2024-08-18 17:24:54 +0530

    Update url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
-export const NEWS_DATA_URL = "https://quickbytes-server.onrender.com/api/news/";
+export const NEWS_DATA_URL = "https://quickbytes-server-js4t.onrender.com/api/news/";
 
 export const NEWS_CATEGORY_DATA_URL =
-  "https://quickbytes-server.onrender.com/api/category/";
+  "https://quickbytes-server-js4t.onrender.com/api/category/";
 
 export const TEJAS_PORTFOLIO_URL = "https://teja-shinde-0612.netlify.app/";
